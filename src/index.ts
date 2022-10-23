80e56530daa5aa7cfc926eedf28c69cb519289a8
7 <a7.@7s-MacBook-Pro.local>
2022-10-23 10:54:46 +0300

    Second commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
+import { Request, Response } from 'express';
 import morgan from 'morgan';
-import route from './routers/route';
+import Router from './routers/route';
+import logger from './utls/logger';
 
 const message: string = 'HEY GUYS';
 console.log(message);
@@ -9,13 +11,13 @@ const app = express();
 
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.status(200).send('My Server is working, HEY THERE');
 });
-
+app.use('/images', logger, Router);
 app.use(morgan('dev'));
 
-app.use('/', route);
+app.use('/', Router);
 
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
--- src/routers/route.ts
+++ src/routers/route.ts
@@ -1,25 +1,25 @@
-import express from "express";
-import { Request, Response } from "express";
-import imgProcess from "../utls/sharpimg";
+import express from 'express';
+import { Request, Response } from 'express';
+import imgProcess from '../utls/sharpimg';
 
 const Router = express.Router();
 
-Router.get("/resize", async (req: Request, res: Response) => {
+Router.get('/resize', async (req: Request, res: Response) => {
   let { imgName, w, h } = req.query as {
     imgName: string;
     w: string;
     h: string;
   };
 
-  if (!imgName) return res.status(404).send("ERR img have invalid params name");
-  if (!w) return res.status(404).send("ERR img have invalid params width");
-  if (!h) return res.status(404).send("ERR img have invalid params height");
+  if (!imgName) return res.status(404).send('ERR img have invalid params name');
+  if (!w) return res.status(404).send('ERR img have invalid params width');
+  if (!h) return res.status(404).send('ERR img have invalid params height');
 
   // it should retun http://localhost:3000/resize?imgName=myLogo4&w=200&h=200
   const sharped = await imgProcess(imgName, w, h);
 
   if (!sharped.sharped)
-    return res.status(400).json("ERR HAndling" + sharped.err);
+    return res.status(400).json('ERR HAndling' + sharped.err);
   return res.status(200).sendFile(sharped.file);
 });
 
